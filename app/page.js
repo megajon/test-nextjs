b8f0765aabd094692a46b2e2bcf42d77da1c30c2
megajon <megajon@megajon.com>
2025-01-08 15:19:22 -0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,18 @@
+'use client'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+
+export default function Home() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <div id="date-container">
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        customInput={<input readOnly />}
+      />
+    </div>
+  );
+}

d895ff9aa59d7f74b6ae284378086a6a340d3c9b
megajon <megajon@megajon.com>
2025-01-08 16:45:44 -0800

    adds readonly to datepick with js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
 'use client'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [startDate, setStartDate] = useState(new Date());
+  useEffect(() => {
+    let inputElements = document.getElementsByTagName("input")
+    let datePicker = inputElements[0]
+    datePicker.setAttribute("readonly", "")
+  }, [])
 
   return (
     <div id="date-container">
       <DatePicker
         selected={startDate}
         onChange={(date) => setStartDate(date)}
-        customInput={<input readonly />}
       />
     </div>
   );
